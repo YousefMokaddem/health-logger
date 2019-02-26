1093a4b4a2db371901b20f7f6f3f3912b1159625
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-25 22:14:30 -0500

    Deleting a day now deletes it's associated foods aswell

--- models/Day.js
+++ models/Day.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Day.associate = function(models) {
     // associations can be defined here
-    Day.hasMany(models.Food);
+    Day.hasMany(models.Food, { onDelete: 'cascade', hooks:true });
     Day.belongsTo(models.User);
   };
   return Day;
