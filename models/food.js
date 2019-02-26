7a8c185e2de1538c6bf5a799d52fe43dd5bd1a7f
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-25 23:44:37 -0500

    Comments explaining amount feild

--- models/food.js
+++ models/food.js
@@ -1,6 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  //values are in grams or milliliters depending on the isSolid option
+  // values are in grams or milliliters depending on the isSolid option
+  // users will be instructed to add foods with nutritional values for 100 g/ml
+  // the "amount" will be added when users add food to days, and will be used as a multiplier
+  // (amount/100) * all of the nutritional values
   const Food = sequelize.define('Food', {
     name: DataTypes.STRING,
     calories: DataTypes.INTEGER,
