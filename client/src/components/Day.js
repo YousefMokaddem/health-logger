c9016057c3955a6b5ce0dfb15fec0cea9a34f912
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 14:08:46 -0500

    create day component to display each of user's days

---
+++ client/src/components/Day.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+
+
+class Day extends Component {
+    state={food:[]}
+
+    componentDidMount(){
+        fetch(`/api/days/${this.props.day.id}`,{headers:this.props.user.headers})
+            .then(res => res.json())
+            .then(day => this.setState({food:day.Food}));
+    }
+
+    showFoods(){
+        if(this.state.food.length > 0){
+            const totalCals = this.state.food.reduce((total, curr)=>{return total + (curr.calories * (curr.amount/100))},0)
+            return(
+                <div>
+                {this.state.food.map(food => {
+                    return(
+                        <div>
+                            <p>{food.amount} of {food.name} = {food.calories * (food.amount/100)}</p> {/*add buttons to edit amounts or remove foods completely*/}
+                        </div>
+                    );
+                })}
+                
+                <p>Total Calories: {totalCals}</p>
+                </div>
+            );
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>{this.props.day.date}</h2>
+                <button onClick={() => this.props.selectDay(this.props.day)}>Select Day</button>
+                {this.showFoods()}
+            </div>
+        );
+    }
+}
+
+export default Day;
--- client/src/components/Days.js
+++ client/src/components/Days.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Day from './Day';
 
 class Days extends Component {
     state={
@@ -14,8 +15,8 @@ class Days extends Component {
     populateDays(){
         return(
             this.state.days.map((day,i) => {
-                return(
-                    <button key={i} onClick={() => this.selectDay(day)}>{day.date}</button>
+                return(//create day component which lists nutrition values, foods, total stats per day in a card with a select day button at the bottom
+                    <Day key={i} selectDay={this.selectDay.bind(this)} user={this.props.user} day={day}/>
                 );
             })
         );
