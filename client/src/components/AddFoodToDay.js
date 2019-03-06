ad599f8c4df327e3a65bd5436968cdd783590437
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 14:07:57 -0500

    add api fetch to AddFoodToDay

--- client/src/components/AddFoodToDay.js
+++ client/src/components/AddFoodToDay.js
@@ -1,9 +1,21 @@
 import React,{Component} from 'react';
 
 class AddFoodToDay extends Component {
+    
+    addFood(e){
+        e.preventDefault();
+        const data = JSON.stringify({amount: e.target[0].value});
+
+        fetch(`/api/days/${this.props.day.id}-${this.props.renderProps.match.params.id}`, {
+            method: "POST",
+            body: data,
+            headers: this.props.user.headers
+        })
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={(e) => this.addFood(e)}>
                 <label htmlFor="amount">Amount</label>
                 <input type="text" name="amount"/>
             </form>
