404214a61f1b2c88b3326573f18cb48718e069e6
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 14:33:50 -0500

    check user before displaying links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-const Header = () => {
-    return(
-        <div>
-            <h1>Health Logger</h1>
+const Header = ({user, day}) => {
+    
+    const checkUser = () => {
+        if(user){
+            return(
             <nav>
                 <NavLink exact to="/">Home</NavLink>
                 <NavLink to="/foods">Foods</NavLink>
-                <NavLink to="/add">Create New Food</NavLink>
+                <NavLink to="/foods/create">New Food</NavLink>
+                <NavLink to="/days">Days</NavLink>
+                <NavLink to="/days/create">New Day</NavLink>
+                <h3>Welcome {user.email}</h3>
             </nav>
+            );
+        }
+    }
+
+    const checkDay = () => {
+        if(day){
+            return(
+                <h3>Selected day: {day.date}</h3>
+            );
+        }
+    }
+
+    return(
+        <div>
+            <h1>Health Logger</h1>
+            {checkUser()}
+            {checkDay()}
         </div>
         
     );
