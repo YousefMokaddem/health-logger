ff5034ad9e744a682a6700d620097450fb4a3d65
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 14:10:05 -0500

    display buttons based on user's association with each food, and based on if a day is selected

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import ReactImageFallback from 'react-image-fallback';
-const Food = ({name, calories, fat, carbs, protein, img, id, unmountFood, index}) => {
+const Food = ({name, calories, fat, carbs, protein, img, id, user, reFetch, authorId, day}) => {
 
     const deleteFood = () => {
         fetch(`/api/foods/${id}`,{
-            method: "DELETE"
+            method: "DELETE",
+            headers: user.headers
+        })
+        .then(res => {
+            if(res.status !== 200){
+                return res.json()
+                .then(res =>
+                    //print error somewhere
+                    1
+                )
+            }else{
+                reFetch();
+            }
         });
-        unmountFood(index);
     }
+
+    const showAddToDayButton = () => {
+        if(day){
+            return (<NavLink to={`/days/addfood/${id}`}><button>Add Food to Day</button></NavLink>);
+        }
+    }
+    const showAuthorButtons = () => {
+        if(user.id === authorId){
+            return(
+                <div>
+                    <button onClick={() => deleteFood()}>Delete</button>
+                    <NavLink to={`/edit/${id}`}><button>Edit</button></NavLink>
+                </div>
+            );
+        }
+    }
+
     return(
         <div className="food-card">
             <ReactImageFallback src={img} fallbackImage="https://www.unesale.com/ProductImages/Large/notfound.png" alt={name}/>
@@ -18,8 +46,8 @@ const Food = ({name, calories, fat, carbs, protein, img, id, unmountFood, index}
             <p>Carbohydrates: {carbs}</p>
             <p>Protein: {protein}</p>
             {/* only show the delete and edit buttons if foods[i].User.id === user.id*/}
-            <button onClick={() => deleteFood()}>Delete</button>
-            <NavLink to={`/edit/${id}`}><button>Edit</button></NavLink>
+            {showAuthorButtons()}
+            {showAddToDayButton()}
         </div>
     );
 }
