3ed137c48196aeed9c33156487e10191e7a7f280
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 21:57:43 -0500

    User persists in local storage

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,35 @@ import AddFood from './components/AddFood';
 
 class App extends Component {
   state = {
-    user: null,
+    user: undefined,
     day: undefined
+  }
+  mounted = false;
+  componentDidMount(){
+    this.mounted = true;
+    if(JSON.parse(localStorage.getItem('user'))){
+      this.setState({
+        user: { 
+          ...JSON.parse(localStorage.getItem('user')),
+          headers: JSON.parse(localStorage.getItem('headers'))
+        }
+      });
+    }else{
+      this.setState({user:null});
+    }
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  signOut(){
+    this.setState({
+      user: null
+    }, () => {
+      localStorage.setItem('user',null);
+      localStorage.setItem('headers',null);
+    });
   }
 
   //store user in state after sign in
@@ -29,28 +56,35 @@ class App extends Component {
       user:{
         email,headers,id
       }
+    }, () => { //set local storage user
+      localStorage.setItem('user', JSON.stringify(this.state.user));
+      let storedHeaders = [];
+      storedHeaders.push(["authorization", headers.get('Authorization')]);
+      storedHeaders.push(["content-type", headers.get('Content-Type')]);
+      localStorage.setItem('headers', JSON.stringify(storedHeaders));
     });
   }
   //add day to state after selected
   selectDay(day){
-    this.setState({day:{...day}});
+      this.setState({day:{...day}});
   }
 
   render() {
+    if(!this.mounted)return(<div></div>);
     return (
       <BrowserRouter>
         <div>
-          <Header day={this.state.day} user={this.state.user}/>
+          <Header signOut={this.signOut.bind(this)} day={this.state.day} user={this.state.user}/>
           <Switch >
             {/* display login screen if not logged in and user info if logged in. */}
-            <Route exact path="/" render={() => <SignIn setUser={this.setUser.bind(this)}/>} />
-            <Route path="/register" render={() => <SignUp />} />
+            <Route exact path="/" render={() => this.state.user? <div></div> : <SignIn setUser={this.setUser.bind(this)}/>} />
+            <Route path="/signup" render={() => <SignUp />} />
             <PrivateRoute user={this.state.user} path="/foods/create" component={({history}) => <CreateFood user={this.state.user} history={history} />} />
             <PrivateRoute user={this.state.user} path="/foods" component={() => <Foods user={this.state.user} day={this.state.day} foods={this.state.foods} />} />
             <PrivateRoute user={this.state.user} path="/edit/:id" component={({match,history}) => <UpdateFood user={this.state.user} match={match} history={history} />} />
             <PrivateRoute user={this.state.user} path="/days/create" component={() => <CreateDay user={this.state.user} />}/>
             <PrivateRoute user={this.state.user} path="/days/addfood/:id" component={(props) => <AddFood renderProps={props} day={this.state.day} user={this.state.user} />}/>
-            <PrivateRoute user={this.state.user} path="/days" component={() => <Days selectDay={this.selectDay.bind(this)} user={this.state.user} />} />
+            <PrivateRoute user={this.state.user} path="/days" component={() => <Days day={this.state.day} selectDay={this.selectDay.bind(this)} user={this.state.user} />} />
             <Route component={NotFound} />
           </Switch>
         </div>
