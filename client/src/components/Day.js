c7c1df3657f1d15b506bf8bdd4cf4d7a443e6e7a
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 21:58:17 -0500

    Add functionality to delete a food item from the day

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -5,6 +5,28 @@ class Day extends Component {
     state={food:[]}
 
     componentDidMount(){
+        this.reFetch();
+    }
+
+    deleteFood(id){
+        fetch(`/api/foods/${id}`,{
+            method: "DELETE",
+            headers: this.props.user.headers
+        })
+        .then(res => {
+            if(res.status !== 200){
+                return res.json()
+                .then(res =>
+                    //print error somewhere or redirect to server error page
+                    1
+                )
+            }else{
+                this.reFetch();
+            }
+        });
+    }
+    
+    reFetch(){
         fetch(`/api/days/${this.props.day.id}`,{headers:this.props.user.headers})
             .then(res => res.json())
             .then(day => this.setState({food:day.Food}));
@@ -18,7 +40,9 @@ class Day extends Component {
                 {this.state.food.map((food, i) => {
                     return(
                         <div key={i}>
-                            <p>{food.amount} of {food.name} = {food.calories * (food.amount/100)}</p> {/*add buttons to edit amounts or remove foods completely*/}
+                            <p>{food.amount} of {food.name} = {food.calories * (food.amount/100)}</p>
+                            <button onClick={() => this.deleteFood(food.id)}>Delete</button>
+                            {/*add buttons to edit amounts or remove foods completely*/}
                         </div>
                     );
                 })}
@@ -33,6 +57,7 @@ class Day extends Component {
         return(
             <div>
                 <h2>{this.props.day.date}</h2>
+                <button onClick={() => this.props.deleteDay(this.props.day)}>Delete Day</button>
                 <button onClick={() => this.props.selectDay(this.props.day)}>Select Day</button>
                 {this.showFoods()}
             </div>
