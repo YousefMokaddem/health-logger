a3114f51bffcfc7bca76b22ea110c33dc7904511
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-06 21:58:38 -0500

    Add function to delete day

--- client/src/components/Days.js
+++ client/src/components/Days.js
@@ -7,16 +7,14 @@ class Days extends Component {
     }
 
     componentDidMount(){
-        fetch('/api/days',{headers:this.props.user.headers})
-            .then(res => res.json())
-            .then(days => this.setState({days}));
+        this.reFetch();
     }
 
     populateDays(){
         return(
             this.state.days.map((day,i) => {
                 return(//create day component which lists nutrition values, foods, total stats per day in a card with a select day button at the bottom
-                    <Day key={i} selectDay={this.selectDay.bind(this)} user={this.props.user} day={day}/>
+                    <Day key={i} deleteDay={this.deleteDay.bind(this)} selectDay={this.selectDay.bind(this)} user={this.props.user} day={day}/>
                 );
             })
         );
@@ -26,9 +24,26 @@ class Days extends Component {
         this.props.selectDay(day);
     }
 
+    deleteDay(day){
+        fetch(`/api/days/${day.id}`, {
+            method: "DELETE",
+            headers: this.props.user.headers
+        }).then(res => {
+            if(res.status === 200)
+                this.reFetch();
+        })
+    }
+
+    reFetch(){
+        fetch('/api/days',{headers:this.props.user.headers})
+            .then(res => res.json())
+            .then(days => this.setState({days}));
+    }
+
     render(){
         return(
             <div>  
+                <h2>Days:</h2>
                 {this.populateDays()}
             </div>
         );
