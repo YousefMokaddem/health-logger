c2d5817d5776f3cb0c6c486ae39ea0737853dbc4
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-26 23:26:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,12 @@ const app = express();
 const morgan = require('morgan');
 const routes = require('./routes');
 const cors = require ('cors');
-const path = require('path')
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', routes);
 
-
-if(process.env.NODE_ENV === 'production'){
-    // set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req,res) => {
-      res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-  
 app.use(cors());
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -37,11 +26,6 @@ app.use((err, req, res, next) => {
     });
 });
 
-// set our port
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 5000);
+const port = 5000;
 
-// start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-  console.log(process.env.OPENSHIFT_NODEJS_PORT);
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
